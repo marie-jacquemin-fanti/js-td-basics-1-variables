1e6f3ce59c636366e3ab7c3d915d2aea26fb31a6
marie-jacquemin-fanti <marie.jacquemin-fanti@student.hepl.be>
2024-09-28 15:25:59 +0200

    Exo à faire

--- starter/script009.js
+++ starter/script009.js
@@ -5,7 +5,6 @@
 // 1. Initialisez les variables firstName à "John" et age à 28.
 const firstName ="John";
 let age =28;
-console.log(firstName + " " + age);
 
 // Conversion de type
 // 2. Affichez la phrase "John a 28 ans" en utilisant les variables et affichez le type de cette expression.
@@ -14,16 +13,14 @@ console.log(`${firstName} a ${age} ans`);
 // 3. Déclarez les variables job et isMarried, puis affectez la valeur "professeur" à job et false à isMarried. Ensuite, affichez la phrase "John est un professeur de 28 ans. Est-il marié ? false".
 let job ="Professeur";
 const isMarried =false;
-console.log(isMarried);
-console.log(`${firstName} est un ${job} de ${age} ans`);
+console.log(`${firstName} est un ${job} de ${age} ans. Est-il marié ? ${isMarried}`);
 
 // Variable mutation
 // 4. Affectez la valeur "vingt-huit" (c'est pas bien de changer le type) à la variable age et la valeur "conducteur" à la variable job. Ensuite, affichez la phrase précédente dans une boîte de dialogue.
 job="Conducteur";
 age="vingt-huit";
-//const titi =alert(`${firstName} est un ${job} de ${age} ans`);
+alert(`${firstName} est un ${job} de ${age} ans. Est-il marié ? ${isMarried}`);
 
 
 // 5. Affichez une boîte de dialogue demandant "Quel est son nom de famille ?" pour permettre à l'utilisateur d'entrer le nom de famille de John. Stockez ce nom dans une variable lastName, puis affichez "Le nom de famille de John est … (le nom)
-//const titi =prompt("Quel est son nom de famille ?");
-/*je ne sais pas ce que je dois faire de "titi"*/
+prompt("Quel est son nom de famille ?");
