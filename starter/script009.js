6e48960cb1f7ab74418c741ca0b63ad8de45b299
marie-jacquemin-fanti <marie.jacquemin-fanti@student.hepl.be>
2024-09-28 15:28:14 +0200

    Exo à faire

--- starter/script009.js
+++ starter/script009.js
@@ -9,6 +9,7 @@ let age =28;
 // Conversion de type
 // 2. Affichez la phrase "John a 28 ans" en utilisant les variables et affichez le type de cette expression.
 console.log(`${firstName} a ${age} ans`);
+//console.log(typeof`${firstName} a ${age} ans`);
 
 // 3. Déclarez les variables job et isMarried, puis affectez la valeur "professeur" à job et false à isMarried. Ensuite, affichez la phrase "John est un professeur de 28 ans. Est-il marié ? false".
 let job ="Professeur";
